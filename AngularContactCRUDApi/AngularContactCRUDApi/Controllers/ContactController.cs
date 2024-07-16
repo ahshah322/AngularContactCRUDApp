b457e9cc4e9c688262c24d070d588e72128193d9
Aɭɩ Hʋsŋʌɩŋ Sʜʌʜ <a.h.shah322@gmail.com>
2024-07-16 18:58:02 +0500

    Simple CRUD operation is performed.

---
+++ AngularContactCRUDApi/AngularContactCRUDApi/Controllers/ContactController.cs
@@ -0,0 +1,62 @@
+﻿using AngularContactCRUDApi.Data;
+using AngularContactCRUDApi.DTOs;
+using AngularContactCRUDApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularContactCRUDApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: api/contact
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = _context.Contacts.ToList();
+            return Ok(contacts);
+        }
+
+        // POST: api/contact
+        [HttpPost]
+        public IActionResult AddContacts(AddContactRequestDTO request)
+        {
+            var addContact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favorite = request.Favorite,
+            };
+
+            _context.Contacts.Add(addContact);
+            _context.SaveChanges();
+
+            return Ok(addContact);
+        }
+
+        // DELETE: api/contact/1
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact(Guid id)
+        {
+            var contact = _context.Contacts.Find(id);
+
+            if (contact is not null)
+            {
+                _context.Contacts.Remove(contact);
+                _context.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ AngularContactCRUDApi/AngularContactCRUDApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AngularContactCRUDApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularContactCRUDApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
